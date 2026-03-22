4dc867693ab1f81572511233707fdd116b4681ec
Corbin Harrison <corbinh93@gmail.com>
2026-03-22 02:01:08 -0700

    Started vacation card component with test data

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,5 +1,10 @@
+//
+//testing purposes only. will be replaced with database
+//
+import { vacations, users } from "../test/testDB"
 
 
+import VacationCard from "../general/VacationCard"
 
 // component for overall homepage structure and logic
 function Home(){
@@ -12,18 +17,50 @@ function Home(){
             <div className="w-full h-[90vh]">
 
 
-                <div className="ml-[13.5%] mr-[13.5%]">
+                <div className="flex flex-col ml-[13.5%] mr-[13.5%] mt-15 gap-10">
 
+
+                    {/* overall mix of vacations */}
                     <div>
 
-                        <h1>All Vacations</h1>
-                        <div>
-                            
+                        <h1 className="text-3xl">Discover</h1>
+
+                        <div className="flex justify-start flex-wrap gap-10 mt-10">
+
+                            {vacations.map(vacation => (
+
+                                <VacationCard id={vacation.id} location={vacation.location} image={vacation?.image} activity={vacation.activity}
+                                hotel={vacation.hotel} flight={vacation.flight} rental={vacation.rental} /> 
+
+                            ))}
+
+                        </div>
+
+                    </div>
+                    
+                    {/*   */}
+                    <div>
+
+                        <h1 className="text-3xl">Relaxation</h1>
+
+                        <div className="flex justify-start flex-wrap gap-10 mt-10">
+
+                
                         </div>
 
                     </div>
 
+                    {/*   */}
+                   <div>
 
+                        <h1 className="text-3xl">Adventure</h1>
+
+                        <div className="flex justify-start flex-wrap gap-10 mt-10">
+
+                          
+                        </div>
+
+                    </div>
 
 
 
